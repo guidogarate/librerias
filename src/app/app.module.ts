8bb81f396e262d6bc9585815a774e626c4fcd899
SAGE-DESA <guido.garate@gmail.com>
2019-12-11 12:31:29 -0400

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from "@angular/core";
 import { APP_ROUTES } from "./app.routes";
 import { MainModule } from "./master/main/main.module";
 
+import { FormsModule } from "@angular/forms";
+
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, APP_ROUTES, MainModule],
+  imports: [BrowserModule, APP_ROUTES, MainModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/master/main/modules/form/select/select.component.ts
+++ src/app/master/main/modules/form/select/select.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { LoginModels } from "./login.models";
 declare function init_select();
 @Component({
   selector: "app-select",
@@ -6,11 +8,28 @@ declare function init_select();
   styleUrls: ["./select.component.css"]
 })
 export class SelectComponent implements OnInit {
+  usuario: LoginModels = new LoginModels("", "", "", null);
+  databases = [
+    {
+      id: 1,
+      nombre: "sagesoft"
+    },
+    {
+      id: 2,
+      nombre: "sagedesa"
+    }
+  ];
+
   constructor() {}
 
   ngOnInit() {
     setTimeout(() => {
       init_select();
     }, 1000);
+  }
+
+  ingresar(form: NgForm) {
+    console.log(this.usuario);
+    console.log(form);
   }
 }
